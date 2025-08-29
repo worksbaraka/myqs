5319de11170954c0e0a0e7a60af2a803766cb3fd
barakahworks <worksbarakah@gmail.com>
2025-08-29 11:53:35 +0300

    0.1.4
    About section updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Navbar from '../components/Navbar';
 import Hero from "../components/Hero";
+import About from "../components/About";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-yellow-100 flex flex-col">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }

4cc716357f618f82df81ac6cf1958fc8ffb54b57
barakahworks <worksbarakah@gmail.com>
2025-08-29 15:35:41 +0300

    0.1.6
    Fix #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
@@ -9,18 +8,6 @@ import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>myQS - Connect with Certified Quantity Surveyors</title>
-        <meta name="description" content="myQS connects contractors, homeowners, and developers with certified Quantity Surveyors across Africa for professional cost management." />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </Head>
       <div className="min-h-screen flex flex-col bg-gradient-to-br from-[#1a2332] to-[#0a1525]">
         <Navbar />
         <Hero />
@@ -30,6 +17,5 @@ export default function Home() {
         <ContactForm />
         <Footer />
       </div>
-    </>
   );
 }

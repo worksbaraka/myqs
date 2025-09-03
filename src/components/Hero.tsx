8ba04874de9cf1d990d3eecd4b38f7bd86cb8781
barakahworks <worksbarakah@gmail.com>
2025-09-03 07:45:56 +0300

    0.1.11
    Fix #10: remove unused useState; Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import styles from './Hero.module.css';
 
 // Configuration types
